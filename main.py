03b268d19ecaf79bd04f82aabe223c352cbbfef4
C23051000 <168742581+C23051000@users.noreply.github.com>
2024-05-08 23:14:13 +0100

    database bug fixed

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from Website import create_app
+from Website import db  # Import db here
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    with app.app_context():
+        # Create all tables
+        db.create_all()
+    app.run(debug=True)
